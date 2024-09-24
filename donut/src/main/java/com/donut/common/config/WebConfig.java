33fcf389690eb87c663e63f9b745bb1a308433e8
jun <ahmin030@naver.com>
2024-09-24 14:14:37 +0900

    [ENV][CORS}CORS Setting

--- donut/src/main/java/com/donut/common/config/WebConfig.java
+++ donut/src/main/java/com/donut/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/graphql")  // GraphQL 엔드포인트에 대해 CORS 설정
-                .allowedOrigins("http://127.0.0.1:5500")  // 프론트엔드 URL을 허용
+                .allowedOriginPatterns("*")  // 프론트엔드 URL을 허용
                 .allowedMethods("POST")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 자격 증명 허용 (예: 쿠키)
