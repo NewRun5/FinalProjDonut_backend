6ee61bf1f2f600acb01a94569353c65bee3898d5
jun <ahmin030@naver.com>
2024-10-02 16:45:27 +0900

    #8 generate curriculum

--- donut/src/main/java/com/donut/common/config/WebSocketConfig.java
+++ donut/src/main/java/com/donut/common/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.donut.common.config;
 
 import com.donut.chapter.questionChatBot.QuestionChatBotHandler;
 import com.donut.curriculum.chatBot.CurriculumChatbotWebSocketHandler;
+import com.donut.refrenceOnly.contentChatBot.langGraph.TestWSHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -14,9 +15,10 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     private final CurriculumChatbotWebSocketHandler curriculumHandler;
     private final QuestionChatBotHandler questionHandler;
+    private final TestWSHandler testWSHandler;
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(curriculumHandler, "/ws-generate-curriculum").setAllowedOrigins("*");
-//        registry.addHandler(questionHandler, "/ws-chat").setAllowedOrigins("*");
+        registry.addHandler(testWSHandler, "/ws-chat").setAllowedOrigins("*");
     }
 }
