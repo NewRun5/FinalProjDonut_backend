ac4aedb6eded24abaaa39dfc693021dae91b5645
mynameisjsoook <jsoook1992@gmail.com>
2024-10-07 15:11:25 +0900

    - 시큐리티 코드 최종 추가

---
+++ donut/src/main/java/com/donut/login/utils/PasswordEncoderUtil.java
@@ -0,0 +1,12 @@
+package com.donut.login.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderUtil {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String rawPassword = "your_password";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        System.out.println("암호화된 비밀번호: " + encodedPassword);
+    }
+}
